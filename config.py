868acba88a571717b4108cd28af0c9298c3fa401
root <root@vs.rottmann-moebel.de>
2014-07-26 11:43:11 +0200

    setup

---
+++ config.py
@@ -0,0 +1,13 @@
+#!/bin/env python3
+
+symlinks = [
+	(sdir + '/css/bootstrap.min.css', cakedir + '/webroot/css/bootstrap.min.css'),
+	(sdir + '/css/bootstrap-theme.min.css', cakedir + '/webroot/css/bootstrap-theme.min.css'),
+	(sdir + '/fonts/glyphicons-halflings-regular.eot', cakedir + '/webroot/css/fonts/glyphicons-halflings-regular.eot'),
+	(sdir + '/fonts/glyphicons-halflings-regular.svg', cakedir + '/webroot/css/fonts/glyphicons-halflings-regular.svg'),
+	(sdir + '/fonts/glyphicons-halflings-regular.ttf', cakedir + '/webroot/css/fonts/glyphicons-halflings-regular.ttf'),
+	(sdir + '/fonts/glyphicons-halflings-regular.woff', cakedir + '/webroot/css/fonts/glyphicons-halflings-regular.woff'),
+	(sdir + '/js/bootstrap.min.js', cakedir + '/webroot/js/bootstrap.min.js'),
+	(sdir + '/js/jquery.min.js', cakedir + '/webroot/js/jquery.min.js'),
+	(sdir + '/js/jquery.min.map', cakedir + '/webroot/js/jquery.min.map'),
+]
